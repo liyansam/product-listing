ef64c83d8ddfca36b4c683d702e1aa3aaa3b3838
liyansam <liyansam@qq.com>
2018-02-08 22:44:00 -0800

    project finished

--- frontend/app/productList/productList.js
+++ frontend/app/productList/productList.js
@@ -104,19 +104,4 @@ angular.module('myApp.productList', ['ngRoute'])
             });
         };
 
-        $scope.formatDate = function(date) {
-            var monthNames = [
-            "January", "February", "March",
-            "April", "May", "June", "July",
-            "August", "September", "October",
-            "November", "December"
-            ];
-
-            var day = date.getDate();
-            var monthIndex = date.getMonth();
-            var year = date.getFullYear();
-
-            return monthNames[monthIndex] + ' ' + day + " " + year;
-        }
-
     }]);
